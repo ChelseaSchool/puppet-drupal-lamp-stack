32c7506a736e465ae305fa3060417042d831759a
rik <rikgoldman@gmail.com>
2014-10-18 14:09:25 -0400

    added admin password

--- drupal-install.sh
+++ drupal-install.sh
@@ -3,8 +3,11 @@
 #######
 # Use Drush to setup drupal at /vagrant/drupal - shared with host.
 # Use Drush to setup database for Drupal - called Drupal - credentials root/root
+# Set Password for admin to admin with drush
 #######
 
 cd /vagrant && sudo drush dl drupal --drupal-project-rename=drupal
 
 cd /vagrant/drupal/ && sudo drush site-install standard --db-url=mysql://root:root@localhost/drupal
+
+cd /vagrant/drupal && sudo drush upwd admin --password="admin"
